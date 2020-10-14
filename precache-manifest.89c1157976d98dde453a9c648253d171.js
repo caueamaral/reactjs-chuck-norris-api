68aee77fa7ed8f64764cdcff9336e4d59a259c66
Caue Amaral de Souza <caue.amaral@hotmail.com>
2020-10-14 16:02:40 -0300

    Updates

---
+++ precache-manifest.89c1157976d98dde453a9c648253d171.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fd464eacad8a93610cc1c9ca816b66",
+    "url": "/caueamaral/reactjs-chuck-norris-api/index.html"
+  },
+  {
+    "revision": "6f9576cbd9747c7eb711",
+    "url": "/caueamaral/reactjs-chuck-norris-api/static/css/main.d11f75f3.chunk.css"
+  },
+  {
+    "revision": "04de323216177ee30cd6",
+    "url": "/caueamaral/reactjs-chuck-norris-api/static/js/2.be9f6a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/caueamaral/reactjs-chuck-norris-api/static/js/2.be9f6a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9576cbd9747c7eb711",
+    "url": "/caueamaral/reactjs-chuck-norris-api/static/js/main.9adc2bfa.chunk.js"
+  },
+  {
+    "revision": "9a1c3815fe57a8bc024d",
+    "url": "/caueamaral/reactjs-chuck-norris-api/static/js/runtime-main.ac42828d.js"
+  },
+  {
+    "revision": "8411ca7a0d1fac2a43cc99c0071b7450",
+    "url": "/caueamaral/reactjs-chuck-norris-api/static/media/chuck-norris.8411ca7a.jpg"
+  }
+]);
